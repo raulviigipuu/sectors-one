765ec8abe1d89cf8c35d61b832cf104743f5af19
Raul Viigipuu <raul@viigipuu.ee>
2022-05-03 10:38:01 +0300

    spring boot project

---
+++ src/main/java/dev/sectorsone/SectorsOneApplication.java
@@ -0,0 +1,13 @@
+package dev.sectorsone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectorsOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectorsOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sectorsone/SectorsOneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sectorsone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectorsOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
